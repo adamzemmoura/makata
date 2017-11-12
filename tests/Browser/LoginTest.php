5792cac6c308f6017882690724e809e6b81093fe
Jose Andrade <jandrade@gmail.com>
2017-11-12 09:24:52 -0500

    Added login test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -6,18 +6,18 @@ use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class ExampleTest extends DuskTestCase
+class LoginTest extends DuskTestCase
 {
     /**
      * A basic browser test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testLogin()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                    ->assertSee('Laravel');
+                    ->assertSee('login');
         });
     }
 }
