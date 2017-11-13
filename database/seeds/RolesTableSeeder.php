e4c97ed3e7e28d10c6e43dec7eb3106d3617d9c9
Jose Andrade <jandrade@gmail.com>
2017-11-12 21:49:10 -0500

    Added seeds

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-<<<<<<< HEAD
 use Carbon\Carbon;
-=======
->>>>>>> be3381b32478dd2ee5fe0f1607e974b719d1f4a5
 
 class RolesTableSeeder extends Seeder
 {
@@ -16,7 +13,6 @@ class RolesTableSeeder extends Seeder
     public function run()
     {
         Eloquent::unguard();
-<<<<<<< HEAD
 
         $faker = Faker\Factory::create();
         
@@ -31,15 +27,3 @@ class RolesTableSeeder extends Seeder
         ]);
     }
 }
-=======
-        
-        DB::table('roles')->insert([
-            'name' => 'admin'
-        ]);
-
-        DB::table('roles')->insert([
-            'name' => 'user'
-        ]);
-    }
-}
->>>>>>> be3381b32478dd2ee5fe0f1607e974b719d1f4a5
